5a6bdb9b42d4ee2a1c244b2fd3cabaf7a220be99
Peter Gerasimenko <gpstmp@gmail.com>
2019-03-29 17:14:12 +0300

    fix(index): replace throw with reject

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const asyncAction = async (id) => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      throw new Error(`${id} throw from async`);
+      return reject(new Error(`${id} throw from async`));
     }, Math.ceil(Math.random() * 1000));
   });
 };
 
 const start = async () => {
   try {
-    await asyncAction(1);
+    await asyncAction(2);
   } catch (error) {
     console.log('catch error:', error);
   }
